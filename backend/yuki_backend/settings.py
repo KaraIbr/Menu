6d4953efc872a944cb77e9a6b98838dd5d003b76
KarinaIbarra <k210240.i@gmail.com>
2026-04-21 14:00:22 -0600

    Update settings.py

--- backend/yuki_backend/settings.py
+++ backend/yuki_backend/settings.py
@@ -120,4 +120,4 @@ CSRF_TRUSTED_ORIGINS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
