9a15750f498754f4a6acf41ff1fe08584c0cac60
KaraIbr <k210240.i@gmail.com>
2026-04-21 17:21:57 -0600

    fix: remove JWT auth requirement

--- backend/yuki_backend/settings.py
+++ backend/yuki_backend/settings.py
@@ -94,9 +94,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
