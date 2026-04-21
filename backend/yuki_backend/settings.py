a1610fdf8b27780daf07216e329a0243425a2119
KaraIbr <k210240.i@gmail.com>
2026-04-21 17:23:01 -0600

    fix: disable CSRF middleware and unauthenticated user

--- backend/yuki_backend/settings.py
+++ backend/yuki_backend/settings.py
@@ -34,7 +34,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -98,6 +97,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
+    'UNAUTHENTICATED_USER': None,
 }
 
 SIMPLE_JWT = {
